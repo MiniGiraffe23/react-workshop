b779c59f9b6db21a0b7a8e44650d3491d4818060
Joe LeBeau <email": null>
2017-05-16 12:34:39 -0500

    LUCNH TIME :)))

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ class Footer extends Component {
   render() {
     return(<div>
       <footer>
-        Footer
+      <span>
+        A purpose of human life, no matter who is controlling it, is to love whoever is around to be loved. - Kurt Vonnegut,
+        </span>
+        <span className='italics'>
+           Some Book
+        </span>
       </footer>
     </div>)
   }
