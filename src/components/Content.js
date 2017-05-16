ee50ceab5e9d6819fff0328acb7d16d115b273f3
Stacy Curry <email": null>
2017-05-16 11:44:42 -0500

    set up header component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
+import Header from './Header.js';
 
 class Content extends Component {
   render(){
     return(<div>
+      <Header />
       and I'm your main content
       </div>)
   }
 }
 
 export default Content;
+
