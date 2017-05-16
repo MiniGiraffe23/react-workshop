66e2811020a5a075d98c18b02a46cb1f81ad748c
Joe LeBeau <email": null>
2017-05-16 12:28:43 -0500

    add content to main section

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,6 +5,18 @@ class Content extends Component {
   render(){
     return(<div>
       <Heading mainHeader='Home' subHeader="Hi, I'm Joe. Web Developer, live music enthusiast, and lifelong learner" />
+      <br/>
+      <div className='FAQ' >
+        <p className='bold'>Where am I located?</p>
+        <p>Chicago, IL</p>
+        <p className='bold'>What do I work in?</p>
+        <p>Ruby on Rails, React, Javascript & HTML/CSS</p>
+        <p className='bold'>What is my favorite band?</p>
+        <p>Sound Tribe Sector 9</p>
+      </div>
+            <br/>
+
+      <p>This is the body paragraph of my content component. Once this app is actually functional, there will be actual info about me here instead of this placeholder. This is the body paragraph of my content component. Once this app is actually functional, there will be actual info about me here instead of this placeholder. This is the body paragraph of my content component. Once this app is actually functional, there will be actual info about me here instead of this placeholder.</p>
       </div>)
   }
 }
