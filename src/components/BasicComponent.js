0883368036c09a611d910981e88b60ba8b649d0c
Joe LeBeau <email": null>
2017-05-16 11:19:00 -0500

    create barebones component skeleton structure

--- src/components/BasicComponent.js
+++ src/components/BasicComponent.js
@@ -3,9 +3,19 @@ import React, {Component} from 'react'
 class BasicComponent extends Component {
   render(){
     return(<div>
-        Hi, I'm a Basic Component!
+        Hi, Im a Basic Component!
       </div>)
   }
 }
 
 export default BasicComponent;
+
+// 1) Page
+// 2) Header
+// 3) Nav
+// 4) Button/link
+// 5) Main
+// 6) Footer
+// 7) Sidebar
+// 8) Heading
+// 9) Paragraph
